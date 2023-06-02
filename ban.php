30c1336a044c9ea0462354cd142ad3e27d68dd1e
Flaw5252 <94564997+Flaw5252@users.noreply.github.com>
2023-06-02 12:57:51 -0400

    Update ban.php

--- ban.php
+++ ban.php
@@ -7,7 +7,7 @@ include 'ban.php';
 
 
 $userIP = $_SERVER['REMOTE_ADDR'];
-$bannedIPs = array('104.225.187.36', '192.168.0.1'); // Example banned IP addresses
+$bannedIPs = array('104.225.187.36', '174.208.32.180'); // Example banned IP addresses
 if (in_array($userIP, $bannedIPs)) {
     // Implement the ban: Return an error message or redirect the user to a specific page
     echo "Access denied. Your IP address is banned.";
