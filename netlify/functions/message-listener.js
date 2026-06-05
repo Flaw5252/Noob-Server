cac7a85bb39a0b6c288deff26275a7d0c8f546bd
Flaw5252 <94564997+Flaw5252@users.noreply.github.com>
2026-06-05 09:43:15 -0400

    Delete netlify/functions directory

--- netlify/functions/message-listener.js
+++
@@ -1,7 +0,0 @@
-import { neon, neonConfig } from '@neondatabase/serverless';
-import { WebSocket } from 'ws';
-
-neonConfig.webSocketConstructor = WebSocket;
-neonConfig.poolQueryViaFetch = true;
-
-const sql = neon(process.env.DATABASE_URL);
